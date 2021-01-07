542c5a643e45f6824613502278c99c14277e44f9
Fredson Chaves <fredson.ribeiro@ceuma.br>
2021-01-07 18:00:11 -0300

    Add model recipe

--- app/db/models.py
+++ app/db/models.py
@@ -1,5 +1,6 @@
 from . import db
 from datetime import datetime
+from sqlalchemy.dialects.postgresql import ARRAY
 
 
 class File(db.Model):
@@ -21,3 +22,16 @@ class Chef(db.Model):
 
     file = db.relationship("File", foreign_keys=file_id)
 
+
+class Recipe(db.Model):
+    __tablename__ = 'FRECIPE'
+    id = db.Column('ID', db.Integer, primary_key=True)
+    name = db.Column('NAME', db.String(100), nullable=False)
+    chef_id = db.Column('CHEFID', db.Integer, db.ForeignKey('FCHEF.ID'))
+    ingredients= db.Column('INGREDIENTS', ARRAY(db.Text), nullable=False)
+    preparations = db.Column('PREPARATIONS', ARRAY(db.Text), nullable=False)
+    adicional_information = db.Column('ADDICIONALINFORMATIONS', db.Text)
+    created_at = db.Column('CREATEDAT', db.DateTime, nullable=False, default=datetime.utcnow)
+    modified_at = db.Column('MODIFIEDAT', db.DateTime, nullable=False, default=datetime.utcnow)
+
+    chef = db.relationship("File", foreign_keys=chef_id)
