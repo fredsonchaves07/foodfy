1325249076dee251d13aed652debd0ecc4c9dafc
Fredson Chaves <fredsonchaves07@gmail.com>
2021-03-09 19:57:58 -0300

    fix: Added removed script

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,4 +1,33 @@
 // TODO -> refatorar todo os scripts
+function addIngredient(){
+    const ingredients = document.querySelector('#ingredients')
+    const fieldContainer = document.querySelectorAll(".ingredient")
+    const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true)
+
+    if(newField.children[0].value == ""){
+        return false
+    }
+
+    newField.children[0].value = ""
+            
+        ingredients.appendChild(newField)
+    }
+
+function addPreparation(){
+    const preparations = document.querySelector('#preparations')
+    const fieldContainer = document.querySelectorAll(".preparation")
+    const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true)
+
+    if(newField.children[0].value == ""){
+        return false
+    }
+
+    newField.children[0].value = ""
+
+    preparations.appendChild(newField)
+            
+}
+
 const modal = document.querySelector('.modal-background')
 const receitas = document.querySelectorAll('.receitas')
 
