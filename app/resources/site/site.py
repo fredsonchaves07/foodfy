6fcc89a6fb199fc9b3c9a67e644617bb944338da
Fredson Chaves <fredsonchaves07@gmail.com>
2021-03-23 19:35:29 -0300

    feat: chefs page

--- app/resources/site/site.py
+++ app/resources/site/site.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, request, render_template, redirect, url_for
 from app.controllers.admin import recipe as recipe_controller
+from app.controllers.admin import chef as chef_controller
 
 
 site = Blueprint('site', __name__, url_prefix='/')
 
 
-@site.route('/about', methods=['GET'])
+@site.route('/', methods=['GET'])
 def index():
     return render_template('site/about.html')
 
@@ -15,3 +16,10 @@ def recipes():
     recipes = recipe_controller.list_recipes()
     
     return render_template('site/recipe.html', recipes=recipes)
+
+
+@site.route('/chefs', methods=['GET'])
+def chefs():
+    chefs = chef_controller.list_chefs()
+    
+    return render_template('site/chef.html', chefs=chefs)
