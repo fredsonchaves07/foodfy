6c956a46ff896a32e96a36aeead391f6c99b98ee
Fredson Chaves <fredsonchaves07@gmail.com>
2021-03-23 19:17:13 -0300

    feat: recipes list pages

--- app/resources/site/site.py
+++ app/resources/site/site.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, render_template, redirect, url_for
-from app.controllers.site import chef as chef_controller
+from app.controllers.admin import recipe as recipe_controller
 
 
 site = Blueprint('site', __name__, url_prefix='/')
@@ -8,3 +8,10 @@ site = Blueprint('site', __name__, url_prefix='/')
 @site.route('/about', methods=['GET'])
 def index():
     return render_template('site/about.html')
+
+
+@site.route('/recipes', methods=['GET'])
+def recipes():
+    recipes = recipe_controller.list_recipes()
+    
+    return render_template('site/recipe.html', recipes=recipes)
