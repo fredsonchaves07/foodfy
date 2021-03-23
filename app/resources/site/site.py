c14be8f75ae5ecf19d74523aa9b145cbe13063f4
Fredson Chaves <fredsonchaves07@gmail.com>
2021-03-23 19:44:07 -0300

    feat: index page

--- app/resources/site/site.py
+++ app/resources/site/site.py
@@ -8,6 +8,13 @@ site = Blueprint('site', __name__, url_prefix='/')
 
 @site.route('/', methods=['GET'])
 def index():
+    recipes = recipe_controller.list_recipes()
+    
+    return render_template('site/index.html', recipes=recipes)
+
+
+@site.route('/about', methods=['GET'])
+def about():
     return render_template('site/about.html')
 
 
