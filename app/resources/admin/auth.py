082521df46e8309ae31b9af36d8022648a8b6a67
Fredson Chaves <fredsonchaves07@gmail.com>
2021-04-12 15:12:47 -0300

    feat: login page html and css

--- app/resources/admin/auth.py
+++ app/resources/admin/auth.py
@@ -2,6 +2,6 @@ from flask import Blueprint, request, render_template, redirect, url_for
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login', methods=['GET'])
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return 'oi'
+    return render_template('admin/login.html')
