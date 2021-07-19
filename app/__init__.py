4cb7bb5f487e2234844288e79e92f7036c57bb7e
Fredson Chaves <fredsonchaves07@gmail.com>
2021-07-19 14:05:21 -0300

    feat: migrate config

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,17 @@
 from dynaconf import FlaskDynaconf
 from flask import Flask
 
-from app.ext import api, db
+from app.ext import api, db, migrate
 
 dynaconf = FlaskDynaconf()
 
 
 def create_app():
     app = Flask(__name__)
+
     dynaconf.init_app(app)
     db.init_app(app)
+    migrate.init_app(app)
     api.init_app(app)
 
     return app
---
+++ app/ext/migrate.py
@@ -0,0 +1,9 @@
+from app.ext import db
+from app.ext.api import models  # noqa
+from flask_migrate import Migrate
+
+migrate = Migrate()
+
+
+def init_app(app):
+    migrate.init_app(app, db)
