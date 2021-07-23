f829bdc137bc18ab88e8121a4b71ee3e14b5f47f
Fredson Chaves <fredsonchaves07@gmail.com>
2021-07-23 19:50:56 -0300

    fixture pytest database

--- test/conftest.py
+++ test/conftest.py
@@ -1,8 +1,16 @@
 import pytest
 from app import create_app
+from app.ext.database import db
 
 
 @pytest.fixture()
 def app():
     """Inicialize app"""
     return create_app()
+
+
+@pytest.fixture(False)
+def database(app):
+    db.create_all()
+    yield
+    db.drop_all()
