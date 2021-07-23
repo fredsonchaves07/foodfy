07fbf4aee82bc2d056ab71153548851b42ff0a00
Fredson Chaves <fredsonchaves07@gmail.com>
2021-07-23 19:50:23 -0300

    feat: user creation service and search by email

--- app/ext/api/services/users_services.py
+++ app/ext/api/services/users_services.py
@@ -1,5 +1,20 @@
-# from app.ext.api.models.user import User
+from app.ext.api.models.user import User
+from app.ext.database import db
 
 
-def create_user():
-    pass
+def create_user(name, email, password):
+    user = User()
+    user.name = name
+    user.email = email
+    user.password = password
+
+    db.session.add(user)
+    db.session.commit()
+
+    return {"id": user.id, "name": user.name, "email": user.email}
+
+
+def find_by_email(email):
+    user = User.query.filter_by(email=email).all()
+
+    return user
