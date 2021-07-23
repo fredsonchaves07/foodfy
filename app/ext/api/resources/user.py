7928f5922f5550d455ae731c971405f64ae00a99
Fredson Chaves <fredsonchaves07@gmail.com>
2021-07-23 19:49:37 -0300

    feat: black and flake8 formating

--- app/ext/api/resources/user.py
+++ app/ext/api/resources/user.py
@@ -1,10 +1,10 @@
-from app.ext.api.controller import users_controller
+# from app.ext.api.controller import users_controller
 from flask import Blueprint
 
 user_api = Blueprint("user", __name__)
 
 
 @user_api.route("", methods=["POST"])
-def create_user():
-    users_controller.create_user()
+def create_user(**kwargs):
+    # users_controller.create_user()
     return "ok", 201
