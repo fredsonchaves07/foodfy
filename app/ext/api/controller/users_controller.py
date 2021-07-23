a69230891c85038a85719e76f8d6141f3254a156
Fredson Chaves <fredsonchaves07@gmail.com>
2021-07-23 19:48:26 -0300

    feat: creation of user creation controller

--- app/ext/api/controller/users_controller.py
+++ app/ext/api/controller/users_controller.py
@@ -1,6 +1,21 @@
+from app.ext.api.exceptions import AdminPermissionRequired, EmailAlreadyExist
 from app.ext.api.services import users_services
 
 
-def create_user():
-    users_services.create_user()
-    pass
+# TODO -> Generate password token
+# TODO -> Create the email sending service
+# TODO -> Sending the user's token created to the registered email
+def create_user(new_user, admin):
+    if not admin:
+        raise AdminPermissionRequired
+
+    name = new_user["name"]
+    email = new_user["email"]
+    password = new_user["password"]
+
+    user = users_services.find_by_email(email)
+
+    if user:
+        raise EmailAlreadyExist
+
+    return users_services.create_user(name, email, password)
