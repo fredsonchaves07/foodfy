05b2d81422454dd50c6f0b51d9469804c41df958
Fredson Chaves <fredsonchaves07@gmail.com>
2021-07-24 16:18:23 -0300

    fix: error github

--- test/conftest.py
+++ test/conftest.py
@@ -2,6 +2,7 @@ import pytest
 from app import create_app
 from app.ext import config
 from app.ext.database import db
+from dynaconf import settings
 
 
 @pytest.fixture()
@@ -9,6 +10,8 @@ def app():
     """Inicialize app"""
     app = create_app()
     config.init_app(app, FORCE_ENV_FOR_DYNACONF="test")
+    settings.SECRET_KEY = "xd3xa6_xe94x15x8bMxe0xe8xc7qFV"
+
     return app
 
 
