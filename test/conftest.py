6c16a3cd8aa66569f8ac7d2d2bfe004b066e5cc9
Fredson Chaves <fredsonchaves07@gmail.com>
2021-07-24 15:30:11 -0300

    feat: test environment change when running pytest

--- test/conftest.py
+++ test/conftest.py
@@ -1,12 +1,15 @@
 import pytest
 from app import create_app
+from app.ext import config
 from app.ext.database import db
 
 
 @pytest.fixture()
 def app():
     """Inicialize app"""
-    return create_app()
+    app = create_app()
+    config.init_app(app, FORCE_ENV_FOR_DYNACONF="test")
+    return app
 
 
 @pytest.fixture(False)
