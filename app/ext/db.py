db27ba554d3660883cbd57c21e849507f3607e18
Fredson Chaves <43495376+fredsonchaves07@users.noreply.github.com>
2021-07-24 16:20:10 -0300

    Revert "Feature/register user"

--- app/ext/db.py
+++ app/ext/db.py
@@ -5,5 +5,3 @@ db = SQLAlchemy()
 
 def init_app(app):
     db.init_app(app)
-
-    return db
--- app/ext/mail.py
+++
@@ -1,7 +0,0 @@
-from flask_mail import Mail
-
-mail = Mail()
-
-
-def init_app(app):
-    mail.init_app(app)
