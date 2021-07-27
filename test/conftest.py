6bc1671c58f4b0c6ef25e5af03ab1193a4a4f89c
Fredson Chaves <fredsonchaves07@gmail.com>
2021-07-27 20:55:37 -0300

    conf test admin user

--- test/conftest.py
+++ test/conftest.py
@@ -1,5 +1,7 @@
 import pytest
 from app import create_app
+from app.ext.api.models.user import User
+from app.ext.api.services import token_services
 from app.ext.database import db
 from dynaconf import settings
 
@@ -13,8 +15,28 @@ def app():
     return app
 
 
-@pytest.fixture(False)
+@pytest.fixture()
 def database(app):
     db.create_all()
     yield
     db.drop_all()
+
+
+@pytest.fixture()
+def admin_user(database):
+    user = User()
+
+    user.name = "admin user"
+    user.email = "admin@email.com"
+    user.password = "admin"
+    user.is_admin = True
+    user.confirmed = True
+
+    db.session.add(user)
+    db.session.commit()
+
+    token = token_services.generate_token(user.id, user.email)
+
+    admin = {"id": user.id, "email": user.email, "token": token}
+
+    return admin
