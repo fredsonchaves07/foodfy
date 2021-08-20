235359fae0cf358107de483c5f4e5ba3e6267067
Fredson Chaves <fredsonchaves07@gmail.com>
2021-08-20 14:43:43 -0300

    feat: add blueprint recipe api

--- app/ext/api/__init__.py
+++ app/ext/api/__init__.py
@@ -2,6 +2,7 @@ from .controller import error_handler
 from .exceptions import BadRequestError
 from .resources.auth import auth_api
 from .resources.chef import chef_api
+from .resources.recipe import recipe_api
 from .resources.user import user_api
 
 
@@ -9,6 +10,7 @@ def init_app(app):
     app.register_blueprint(user_api, url_prefix="/api/v1/user")
     app.register_blueprint(auth_api, url_prefix="/api/v1/auth")
     app.register_blueprint(chef_api, url_prefix="/api/v1/chef")
+    app.register_blueprint(recipe_api, url_prefix="/api/v1/recipe")
 
     app.register_error_handler(404, error_handler.url_not_found)
     app.register_error_handler(405, error_handler.method_not_allowed)
