e63362ec6c622ae3b08ec58f14966a46f989a68e
fredsonchaves07 <fredsonchaves07@gmail.com>
2021-08-26 21:37:56 -0300

    feat: test recipe without ingredient and preparation mode

--- test/recipe/test_edit_recipe.py
+++ test/recipe/test_edit_recipe.py
@@ -1,7 +1,14 @@
 import json
 from io import BytesIO
 
-from app.ext.api.exceptions import InvalidToken, InvalidUser, MaximumImageCapacityError
+from app.ext.api.exceptions import (
+    ChefNotFound,
+    InvalidToken,
+    InvalidUser,
+    MaximumImageCapacityError,
+    RecipeWithoutIngredient,
+    RecipeWithoutPreparationMode,
+)
 from app.ext.api.services import token_services
 
 
@@ -421,3 +428,234 @@ def test_no_edit_recipe_if_have_maximum_capacity_images(client, admin_user):
     assert response.content_type == "application/json"
     assert response.status_code == MaximumImageCapacityError.code
     assert response.json["message"] == MaximumImageCapacityError.message
+
+
+def test_no_edit_recipe_if_chef_not_already_exist(client, admin_user):
+    new_user1 = {
+        "name": "Usuário teste",
+        "email": "email@email.com",
+        "password": "123456",
+        "admin": False,
+    }
+
+    new_chef1 = {"name": "chef test", "avatar": (BytesIO(b"avatar"), "test.jpg")}
+
+    headers = {
+        "Authorization": admin_user.get("token"),
+        "content-type": "application/json",
+    }
+
+    user1 = client.post(
+        "/api/v1/user",
+        data=json.dumps(new_user1),
+        headers=headers,
+    )
+
+    chef1 = client.post(
+        "/api/v1/chef",
+        data=new_chef1,
+        headers=headers,
+        follow_redirects=True,
+        content_type="multipart/form-data",
+    )
+
+    token = token_services.generate_token(user1.json["id"], user1.json["email"])
+    headers["Authorization"] = token
+
+    new_recipe = {
+        "name": "recipe test",
+        "ingredients": ["Ovo", "Carne de Hamburguer"],
+        "preparation_mode": ["Bata um ovo na frigideira", "Frite a carne"],
+        "additional_information": "",
+        "chef_id": chef1.json["id"],
+        "recipe_imgs": [
+            (BytesIO(b"recipe_imgs"), "test1.jpg"),
+        ],
+    }
+
+    recipe = client.post(
+        "/api/v1/recipe",
+        data=new_recipe,
+        headers=headers,
+        follow_redirects=True,
+        content_type="multipart/form-data",
+    )
+
+    recipe_id = recipe.json["id"]
+
+    update_recipe = {
+        "name": "recipe updated",
+        "chef_id": 10,
+        "ingredients": ["Ovo", "Carne de Hamburguer", "Salada"],
+        "preparation_mode": ["Bata um ovo na frigideira", "Coloque a salada"],
+        "recipe_imgs": [
+            (BytesIO(b"recipe_imgs"), "test-alterado.jpg"),
+            (BytesIO(b"recipe_imgs"), "test-alterado.jpg"),
+            (BytesIO(b"recipe_imgs"), "test-alterado.jpg"),
+        ],
+    }
+
+    response = client.patch(
+        f"/api/v1/recipe/{recipe_id}",
+        data=update_recipe,
+        headers=headers,
+        content_type="multipart/form-data",
+    )
+
+    assert response.content_type == "application/json"
+    assert response.status_code == ChefNotFound.code
+    assert response.json["message"] == ChefNotFound.message
+
+
+def test_no_edit_recipe_if_recipe_without_ingredient(client, admin_user):
+    new_user1 = {
+        "name": "Usuário teste",
+        "email": "email@email.com",
+        "password": "123456",
+        "admin": False,
+    }
+
+    new_chef1 = {"name": "chef test", "avatar": (BytesIO(b"avatar"), "test.jpg")}
+
+    headers = {
+        "Authorization": admin_user.get("token"),
+        "content-type": "application/json",
+    }
+
+    user1 = client.post(
+        "/api/v1/user",
+        data=json.dumps(new_user1),
+        headers=headers,
+    )
+
+    chef1 = client.post(
+        "/api/v1/chef",
+        data=new_chef1,
+        headers=headers,
+        follow_redirects=True,
+        content_type="multipart/form-data",
+    )
+
+    token = token_services.generate_token(user1.json["id"], user1.json["email"])
+    headers["Authorization"] = token
+
+    new_recipe = {
+        "name": "recipe test",
+        "ingredients": ["Ovo", "Carne de Hamburguer"],
+        "preparation_mode": ["Bata um ovo na frigideira", "Frite a carne"],
+        "additional_information": "",
+        "chef_id": chef1.json["id"],
+        "recipe_imgs": [
+            (BytesIO(b"recipe_imgs"), "test1.jpg"),
+        ],
+    }
+
+    recipe = client.post(
+        "/api/v1/recipe",
+        data=new_recipe,
+        headers=headers,
+        follow_redirects=True,
+        content_type="multipart/form-data",
+    )
+
+    recipe_id = recipe.json["id"]
+
+    update_recipe = {
+        "name": "recipe updated",
+        "chef_id": chef1.json["id"],
+        "ingredients": [],
+        "preparation_mode": ["Bata um ovo na frigideira", "Coloque a salada"],
+        "recipe_imgs": [
+            (BytesIO(b"recipe_imgs"), "test-alterado.jpg"),
+            (BytesIO(b"recipe_imgs"), "test-alterado.jpg"),
+            (BytesIO(b"recipe_imgs"), "test-alterado.jpg"),
+        ],
+    }
+
+    response = client.patch(
+        f"/api/v1/recipe/{recipe_id}",
+        data=update_recipe,
+        headers=headers,
+        content_type="multipart/form-data",
+    )
+
+    assert response.content_type == "application/json"
+    assert response.status_code == RecipeWithoutIngredient.code
+    assert response.json["message"] == RecipeWithoutIngredient.message
+
+
+def test_no_edit_recipe_if_recipe_without_preparation_mode(client, admin_user):
+    new_user1 = {
+        "name": "Usuário teste",
+        "email": "email@email.com",
+        "password": "123456",
+        "admin": False,
+    }
+
+    new_chef1 = {"name": "chef test", "avatar": (BytesIO(b"avatar"), "test.jpg")}
+
+    headers = {
+        "Authorization": admin_user.get("token"),
+        "content-type": "application/json",
+    }
+
+    user1 = client.post(
+        "/api/v1/user",
+        data=json.dumps(new_user1),
+        headers=headers,
+    )
+
+    chef1 = client.post(
+        "/api/v1/chef",
+        data=new_chef1,
+        headers=headers,
+        follow_redirects=True,
+        content_type="multipart/form-data",
+    )
+
+    token = token_services.generate_token(user1.json["id"], user1.json["email"])
+    headers["Authorization"] = token
+
+    new_recipe = {
+        "name": "recipe test",
+        "ingredients": ["Ovo", "Carne de Hamburguer"],
+        "preparation_mode": ["Bata um ovo na frigideira", "Frite a carne"],
+        "additional_information": "",
+        "chef_id": chef1.json["id"],
+        "recipe_imgs": [
+            (BytesIO(b"recipe_imgs"), "test1.jpg"),
+        ],
+    }
+
+    recipe = client.post(
+        "/api/v1/recipe",
+        data=new_recipe,
+        headers=headers,
+        follow_redirects=True,
+        content_type="multipart/form-data",
+    )
+
+    recipe_id = recipe.json["id"]
+
+    update_recipe = {
+        "name": "recipe updated",
+        "chef_id": chef1.json["id"],
+        "ingredients": ["Ovo", "Carne de Hamburguer"],
+        "preparation_mode": [],
+        "recipe_imgs": [
+            (BytesIO(b"recipe_imgs"), "test-alterado.jpg"),
+            (BytesIO(b"recipe_imgs"), "test-alterado.jpg"),
+            (BytesIO(b"recipe_imgs"), "test-alterado.jpg"),
+        ],
+    }
+
+    response = client.patch(
+        f"/api/v1/recipe/{recipe_id}",
+        data=update_recipe,
+        headers=headers,
+        content_type="multipart/form-data",
+    )
+
+    assert response.content_type == "application/json"
+    assert response.status_code == RecipeWithoutPreparationMode.code
+    assert response.json["message"] == RecipeWithoutPreparationMode.message
