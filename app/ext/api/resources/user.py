5c682f2232e691083d01378b89352194a40014ca
Fredson Chaves <fredsonchaves07@gmail.com>
2021-12-28 16:44:55 -0300

    feat: add user swagger doc

--- app/ext/api/resources/user.py
+++ app/ext/api/resources/user.py
@@ -70,6 +70,21 @@ def create_user(**kwargs):
 @authentication
 @admin_required
 def list_user(**kwargs):
+    """
+    user list endpoint
+    ---
+    tags:
+      - User
+    responses:
+      200:
+        description: User created successfully
+        schema:
+          $ref: '#/definitions/User'
+      401:
+        description: Operation not allowed. Consult the administrator
+      498:
+        description: Expired or invalid token.
+    """
     users = users_controller.list_user()
 
     return users, 200
@@ -80,6 +95,45 @@ def list_user(**kwargs):
 @admin_required
 @audit_log
 def delete_user(id, **kwargs):
+    """
+    user delete endpoint
+    ---
+    tags:
+      - User
+    parameters:
+      - name: id
+        in: path
+        required: true
+        type: string
+        description:
+          User id
+        schema:
+          id: User
+          properties:
+            name:
+              type: string
+              example: "Teste"
+            email:
+              type: string
+              example: "Teste@email.com"
+            password:
+              type: string
+              example: "1234"
+            admin:
+              type: boolean
+              example: false
+    responses:
+      200:
+        description: User created successfully
+        schema:
+          $ref: '#/definitions/User'
+      401:
+        description: Operation not allowed. Consult the administrator
+      404:
+        description: User not found
+      498:
+        description: Expired or invalid token.
+    """
     users_controller.delete_user(id)
 
     return {}, 204
@@ -89,6 +143,45 @@ def delete_user(id, **kwargs):
 @authentication
 @user_self_required
 def get_user(id, **kwargs):
+    """
+    user get endpoint
+    ---
+    tags:
+      - User
+    parameters:
+      - name: id
+        in: path
+        required: true
+        type: string
+        description:
+          User id
+        schema:
+          id: User
+          properties:
+            name:
+              type: string
+              example: "Teste"
+            email:
+              type: string
+              example: "Teste@email.com"
+            password:
+              type: string
+              example: "1234"
+            admin:
+              type: boolean
+              example: false
+    responses:
+      200:
+        description: User created successfully
+        schema:
+          $ref: '#/definitions/User'
+      401:
+        description: Operation not allowed. Consult the administrator
+      404:
+        description: User not found
+      498:
+        description: Expired or invalid token.
+    """
     user = users_controller.get_user(id)
 
     return user, 200
@@ -99,6 +192,49 @@ def get_user(id, **kwargs):
 @user_self_required
 @audit_log
 def update_user(id, **kwargs):
+    """
+    user update endpoint
+    ---
+    tags:
+      - User
+    parameters:
+      - name: id
+        in: path
+        required: true
+        type: string
+        description:
+          User id
+      - name: user data
+        in: body
+        schema:
+          id: User
+          properties:
+            name:
+              type: string
+              example: "Teste"
+            email:
+              type: string
+              example: "Teste@email.com"
+            password:
+              type: string
+              example: "1234"
+            admin:
+              type: boolean
+              example: false
+    responses:
+      200:
+        description: User created successfully
+      400:
+        description: Invalid parameters in request
+      401:
+        description: Operation not allowed. Consult the administrator
+      404:
+        description: User not found
+      422:
+        description: Email already exist
+      498:
+        description: Expired or invalid token.
+    """
     try:
         user_data = UpdateUserSchema(**request.get_json())
     except ValidationError:
