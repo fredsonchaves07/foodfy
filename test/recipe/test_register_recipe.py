c679c02be03cedfe338f92022d83279a48a059da
Fredson Chaves <fredsonchaves07@gmail.com>
2022-01-05 12:38:30 -0300

    feat: add recipe schema test

--- test/recipe/test_register_recipe.py
+++ test/recipe/test_register_recipe.py
@@ -2,6 +2,7 @@ import json
 from io import BytesIO
 
 from app.ext.api.exceptions import (
+    InvalidParameters,
     InvalidToken,
     MaximumImageCapacityError,
     RecipeWithoutIngredient,
@@ -297,3 +298,58 @@ def test_no_register_recipe_if_user_not_authenticated(client, admin_user):
 
     assert response.status_code == InvalidToken.code
     assert response.json["message"] == InvalidToken.message
+
+
+def test_no_register_recipe_with_params_invalid(client, admin_user):
+    new_user1 = {
+        "name": "Usuário teste",
+        "email": "email@email.com",
+        "password": "123456",
+        "admin": False,
+    }
+
+    headers = {
+        "Authorization": admin_user.get("token"),
+        "content-type": "application/json",
+    }
+
+    user = client.post(
+        "/api/v1/user",
+        data=json.dumps(new_user1),
+        headers=headers,
+    )
+
+    new_chef = {"name": "chef test", "avatar": (BytesIO(b"avatar"), "test.jpg")}
+
+    chef = client.post(
+        "/api/v1/chef",
+        data=new_chef,
+        headers=headers,
+        follow_redirects=True,
+        content_type="multipart/form-data",
+    )
+
+    token = token_services.generate_token(user.json["id"], user.json["email"])
+    headers["Authorization"] = token
+
+    recipe = {
+        "ingredients": ["Ovo", "Carne de Hamburguer"],
+        "preparation_mode": ["Bata um ovo na frigideira", "Frite a carne"],
+        "additional_information": "",
+        "chef_id": chef.json["id"],
+        "recipe_imgs": [
+            (BytesIO(b"recipe_imgs"), "test1.jpg"),
+            (BytesIO(b"recipe_imgs"), "test2.jpg"),
+        ],
+    }
+
+    response = client.post(
+        "/api/v1/recipe",
+        data=recipe,
+        headers=headers,
+        follow_redirects=True,
+        content_type="multipart/form-data",
+    )
+
+    assert response.status_code == InvalidParameters.code
+    assert response.json["message"] == InvalidParameters.message
